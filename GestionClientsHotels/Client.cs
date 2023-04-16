d617928ec3f221f7020217f572c36521b1e0bf99
Mohamed islem ABDOU <MohamedislemABDOU@gmail.com>
2023-04-15 23:18:27 -0400

    Ajout de la classe Client

---
+++ GestionClientsHotels/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClientsHotels
+{
+    internal class Client
+    {
+        // Declaration des attributs
+        string nom, prénom, courriel, carteCrédit;
+        int numeroChambre;
+
+        //Creation des propriétés
+        public string Nom { get; set; }
+        public string Prénom { get; set; }
+        public string Courriel { get;set; }
+        public string CarteCrédit { get; set; }
+        public int NumeroChambre { get;set; }
+
+        // Creation du constructeur
+
+        public Client(string nom, string prenom, string courriel, string cartcredit, int numChambre)
+        {
+            this.Nom = nom;
+            this.Prénom= prenom;
+            this.Courriel= courriel;
+            this.CarteCrédit= cartcredit;
+            this.NumeroChambre= numChambre;
+
+        }
+
+    }
+}
